3526b57c23a9f83308d803100d1f130fc56ec1ea
junhwan <ujunhwan@gmail.com>
2021-11-03 23:35:49 +0900

    init

---
+++ src/main/java/boilerplate/springbootdocker/SpringbootDockerApplication.java
@@ -0,0 +1,31 @@
+package boilerplate.springbootdocker;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@SpringBootApplication
+@RestController
+public class SpringbootDockerApplication {
+
+	@GetMapping("/")
+	public String hello() {
+		return "Hello World!";
+	}
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringbootDockerApplication.class, args);
+		System.out.println(" _______  _______  _______           _______  _______    _______ _________ _______  _______ _________\n" +
+				"(  ____ \\(  ____ \\(  ____ )|\\     /|(  ____ \\(  ____ )  (  ____ \\\\__   __/(  ___  )(  ____ )\\__   __/\n" +
+				"| (    \\/| (    \\/| (    )|| )   ( || (    \\/| (    )|  | (    \\/   ) (   | (   ) || (    )|   ) (   \n" +
+				"| (_____ | (__    | (____)|| |   | || (__    | (____)|  | (_____    | |   | (___) || (____)|   | |   \n" +
+				"(_____  )|  __)   |     __)( (   ) )|  __)   |     __)  (_____  )   | |   |  ___  ||     __)   | |   \n" +
+				"      ) || (      | (\\ (    \\ \\_/ / | (      | (\\ (           ) |   | |   | (   ) || (\\ (      | |   \n" +
+				"/\\____) || (____/\\| ) \\ \\__  \\   /  | (____/\\| ) \\ \\__  /\\____) |   | |   | )   ( || ) \\ \\__   | |   \n" +
+				"\\_______)(_______/|/   \\__/   \\_/   (_______/|/   \\__/  \\_______)   )_(   |/     \\||/   \\__/   )_(   \n" +
+				"                                                                                                     ");
+	}
+
+}
---
+++ src/test/java/boilerplate/springbootdocker/SpringbootDockerApplicationTests.java
@@ -0,0 +1,13 @@
+package boilerplate.springbootdocker;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringbootDockerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
